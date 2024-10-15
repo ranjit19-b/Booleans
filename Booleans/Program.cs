8929e7e36a87c5b6fd489fc0c7cb64a7e0c712fe
ranjit19-b <ranjit1041997kumar@gmail.com>
2024-10-15 21:27:26 +0530

    Add files via upload

---
+++ Booleans/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace Booleans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCSharp = true;
+            bool isMono = false;
+            Console.WriteLine(isCSharp);
+            Console.WriteLine(isMono);
+
+            int x = 10;
+            int y = 5;
+            Console.WriteLine(x > y);
+            Console.WriteLine(y > x);
+            Console.WriteLine((x == 10));
+            Console.WriteLine(x == y);
+
+            int myAge = 27;
+            int votingAge = 18;
+            if (myAge >= votingAge)
+            {
+                Console.WriteLine("Elegible for vote.");
+            }
+            else
+            {
+                Console.WriteLine("Not elegible for vote.");
+            }
+        }
+    }
+}
